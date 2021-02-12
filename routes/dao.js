9ecb0ab6b5c33a15bd40855a3eb584932f12358b
Artem <d8r4llik4@gmail.com>
2021-02-12 05:08:16 +0400

    owow

--- routes/dao.js
+++ routes/dao.js
@@ -1,7 +1,10 @@
+const express = require('express')
 const { Router } = require('express')
 const bodyParser = require("body-parser");
 const UserResult = require('../models/UserResult')
 const router = Router()
+
+const app = express()
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
